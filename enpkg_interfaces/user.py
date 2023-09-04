838978608f309c898c11a18f21bba819709fde52
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-09-04 15:14:30 +0200

    Added more APIs to user interface

--- enpkg_interfaces/user.py
+++ enpkg_interfaces/user.py
@@ -2,3 +2,19 @@
 
 class User:
     """Abstract class to represent a user."""
+
+    def is_administrator(self):
+        """Return True if the user is an administrator."""
+        raise NotImplementedError(
+            "Abstract method 'is_administrator' must be implemented in subclass. "
+            f"It was not implemented in {self.__class__.__name__}."
+        )
+    
+    def is_moderator(self):
+        """Return True if the user is a moderator."""
+        raise NotImplementedError(
+            "Abstract method 'is_moderator' must be implemented in subclass. "
+            f"It was not implemented in {self.__class__.__name__}."
+        )
+    
+    
