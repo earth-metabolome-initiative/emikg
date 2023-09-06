7e54a871a4d8aada38b6fb01313a1dd08d072a84
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-09-06 13:59:04 +0200

    Refactored position of interfaces package and started to document deployment

--- enpkg_interfaces/setup.py
+++ enpkg_interfaces/setup.py
@@ -3,10 +3,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='enpkg_interfaces',
-    version="0.0.1",
+    version="0.0.2",
     description='Module providing interfaces for the ENPKG project.',
     url='https://github.com/enpkg/enpkg',
-    keywords='Graph Representation Learning,LINE,TransE,Node2Vec,DeeWalk',
     author="Luca Cappelletti", # Other authors can be added here
     license='MIT',
     python_requires='>=3.8.0',
