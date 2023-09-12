4d706173a07a27872e7e4ca9463496e726a86a07
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-09-12 15:43:01 +0200

    Put togheter translations popup

---
+++ components/form_test/script.js
@@ -0,0 +1,4 @@
+// Form
+// ----
+// This is a simple form component that can be used to create a form
+// from a JSON schema.
--- components/translations_test/script.js
+++ components/translations_test/script.js
@@ -16,13 +16,81 @@
 //
 // The widjet has the ID "translation_widjet".
 
+/**
+ * On click of the "translation_item" element, we set the value of the textarea
+ * and set the class of the translation item as "selected", removing the class
+ * from the previously selected translation item. Additionally, we set the save
+ * button as disabled until any edit is made to the textarea.
+ */
+function selectTranslationItem(translation_item) {
+    var textarea = $("#current_translation");
+    var translation_label = translation_item.attr("reference-label");
+    var textual_item = $("[data-label='" + translation_label + "']");
+    textarea.val(textual_item.text());
+    $(".selected_translation_item").removeClass("selected_translation_item");
+    translation_item.addClass("selected_translation_item");
+    $("#save_translation").addClass("disabled");
+
+    // If the translation item happens to not be visible because
+    // of the scroll, we scroll to it.
+    var translations_list = $("#translations_list");
+    var translation_item_position = translation_item.position();
+    var translation_item_top = translation_item_position.top;
+    var translation_item_height = translation_item.height();
+    var translations_list_height = translations_list.height();
+    var translations_list_top = translations_list.scrollTop();
+    var translations_list_bottom = translations_list_top + translations_list_height;
+    var translation_item_bottom = translation_item_top + translation_item_height;
+    if (translation_item_top < translations_list_top) {
+        translations_list.scrollTop(translation_item_top);
+    } else if (translation_item_bottom > translations_list_bottom) {
+        translations_list.scrollTop(translation_item_bottom - translations_list_height);
+    }
+
+    textarea.focus();
+}
+
+/**
+ * Pupulate the translations widget's translations_list with elements with the class "translatable".
+ */
+function populateTranslationsList() {
+    var translations = $(".translatable");
+    var translations_list = $("#translations_list");
+    translations_list.empty();
+    var first = true;
+    translations.each(function() {
+        var translation = $(this);
+        var translation_label = translation.attr("data-label");
+        var translation_item = $("<li></li>");
+
+        // We set an attribute "reference-label" on the translation_item
+        // so that we can easily retrieve the translation_item
+        // when the user clicks on it.
+        translation_item.attr("reference-label", translation_label);
+
+        translation_item.text(translation_label);
+        translations_list.append(translation_item);
+
+        // We set the onclick event of the translation_item
+        // to call the selectTranslationItem function if the
+        // translation_item is clicked.
+        translation_item.click(function() {
+            selectTranslationItem(translation_item);
+        });
+
+        if (first) {
+            first = false;
+            selectTranslationItem(translation_item);
+        }
+    });
+}
 
 /**
  * Displayes the translations widget.
  */
 function displayTranslationsWidget() {
     var widget = $("#translation_widjet");
-    console.log("displayTranslationsWidget");
+    populateTranslationsList();
     widget.show();
 }
 
@@ -31,7 +99,6 @@ function displayTranslationsWidget() {
  */
 function hideTranslationsWidget() {
     var widget = $("#translation_widjet");
-    console.log("hideTranslationsWidget");
     widget.hide();
 }
 
@@ -95,6 +162,12 @@ function showTranslationsWidgetOnTripleClick() {
             clearTimeout(timeout);
             clicks = 0;
             displayTranslationsWidget();
+
+            // We click on the translation_item that has the same "reference-label"
+            // as the element that was clicked.
+            var translation_label = $(this).attr("data-label");
+            var translation_item = $("li[reference-label='" + translation_label + "']");
+            translation_item.click();
         }
     });
 }
@@ -110,6 +183,16 @@ function toggleTranslationsWidgetOnHotKeys() {
     });
 }
 
+/** 
+ * When there is any keystroke to the textarea with ID "current_translation",
+ * we enable the save button with ID "save_translation".
+ */
+function enableSaveButtonOnKeystroke() {
+    $("#current_translation").keyup(function() {
+        $("#save_translation").removeClass("disabled");
+    });
+}
+
 /**
  * Enables all of the events described above upon the document being ready.
  */
@@ -119,4 +202,5 @@ $(document).ready(function() {
     hideTranslationsWidgetOnCloseButtonClick();
     showTranslationsWidgetOnTripleClick();
     toggleTranslationsWidgetOnHotKeys();
+    enableSaveButtonOnKeystroke();
 });
